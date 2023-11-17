cc55f0a39b21a1bcf93b3cba387284b26de40f28
Иван Ищенко <ivanisenko@MacBook-Air.local>
2023-11-17 18:12:19 +0200

    finished responsive header

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let closeBtn = document.querySelector(".close-btn");
+let navPanel = document.querySelector(".header__nav");
+let hamburgerBtn = document.querySelector(".humburger-btn");
+let body = document.querySelector('body');
+
+hamburgerBtn.addEventListener('click', ()=>{
+    navPanel.classList.add('show');
+})
+
+closeBtn.addEventListener('click', ()=>{
+    navPanel.classList.remove('show');
+})
