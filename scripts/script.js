d62ab9991142529e3e4a55c86f0a68e16318d241
Иван Ищенко <ivanisenko@MacBook-Air.local>
2023-11-29 12:04:38 +0200

    finished responsive experiance block

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ let closeBtn = document.querySelector(".close-btn");
 let navPanel = document.querySelector(".header__nav");
 let hamburgerBtn = document.querySelector(".humburger-btn");
 let body = document.querySelector('body');
+let experianceTopic = document.querySelector(".experiance__topic");
+let experianceLinks = document.querySelectorAll(".experiance__topic-link");
 
 hamburgerBtn.addEventListener('click', ()=>{
     navPanel.classList.toggle('show');
@@ -10,3 +12,19 @@ hamburgerBtn.addEventListener('click', ()=>{
 closeBtn.addEventListener('click', ()=>{
     navPanel.classList.remove('show');
 })
+
+experianceTopic.addEventListener('click', (e)=>{
+    for (i=0; i<experianceLinks.length; i++){
+        if(experianceLinks[i].classList.contains('active-topic-link')){
+            experianceLinks[i].classList.remove('active-topic-link');
+        }
+        if(e.target === experianceLinks[i]){
+            experianceLinks[i].classList.add('active-topic-link');
+        }
+    }
+})
+
+
+
+
+
